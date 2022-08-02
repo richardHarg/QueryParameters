03256dd7d5ff076a6105d88fe65a43e5875f8523
richardHarg <54716924+richardHarg@users.noreply.github.com>
2022-08-02 10:25:34 +0100

    Added seperate project for DynamicLinq Iqueryable Where/OrderBy operations

---
+++ RLH.QueryParameters.DynamicLINQ/QueryableExtensions.cs
@@ -0,0 +1,36 @@
+﻿using RLH.QueryParameters.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
+
+namespace RLH.QueryParameters.DynamicLINQ
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> DynamicWhere<T>(this IQueryable<T> collection,IQueryingParameters queryingParameters)
+        {
+            if (queryingParameters.WhereConditions.Any())
+            {
+                return collection.Where(queryingParameters.BuildDynamicWhereString());
+            }
+            else
+            {
+                return collection;
+            }
+        }
+        public static IQueryable<T> DynamicOrderBy<T>(this IQueryable<T> collection, IQueryingParameters queryingParameters)
+        {
+            if (queryingParameters.OrderByConditions.Any())
+            {
+                return collection.OrderBy(queryingParameters.BuildDynamicOrderByString());
+            }
+            else
+            {
+                return collection;
+            }
+        }
+    }
+}
